d431323e25a2bfebf1fec446d88327fe66e37e1a
Vladimir Goshev <sunx_gh@sunx.name>
2016-11-01 23:32:17 +0000

    Add more options to docker-run.sh helper script

--- docker-run.sh
+++ docker-run.sh
@@ -11,6 +11,10 @@ CONTAINER_NAME="seafile"
 RESTART_POLCY="unless-stopped"
 #Some extra arguments. Like -d ant -ti
 EXTRA_ARGS="-d -ti"
+#docker command. You can use "sudo docker" if you need so
+DOCKER="docker"
+#Extra args to docker command. Like using remote dockerd or something else
+DOCKER_ARGS=""
 
 #You can change default values by adding them to config file ~/.docker-sunx-seafile
 [ -f ~/.docker-sunx-seafile ] && . ~/.docker-sunx-seafile
@@ -19,4 +23,4 @@ EXTRA_ARGS="-d -ti"
 [ ! -z "$CONTAINER_NAME" ]     && CONTAINER_NAME="--name=$CONTAINER_NAME"
 [ ! -z "$RESTART_POLCY" ]      && RESTART_POLCY="--restart=$RESTART_POLCY"
 
-docker run -v $VOLUME_PATH:/home/seafile -p 127.0.0.1:8000:8000 -p 127.0.0.1:8082:8082 $CONTAINER_HOSTNAME $CONTAINER_NAME $RESTART_POLCY $EXTRA_ARGS sunx/seafile
+$DOCKER $DOCKER_ARGS run -v $VOLUME_PATH:/home/seafile -p 127.0.0.1:8000:8000 -p 127.0.0.1:8082:8082 $CONTAINER_HOSTNAME $CONTAINER_NAME $RESTART_POLCY $EXTRA_ARGS sunx/seafile
