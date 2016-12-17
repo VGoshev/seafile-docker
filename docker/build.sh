9760849030f109320ee7f1809c605a53520efa5d
Vladimir A. Goshev (sunx.me) <sunx_git@sunx.name>
2016-12-17 19:21:15 +0100

    Change build script
    * Add version support for libevhtp anf libsearpc
    * Add seafobj - seafile python bindings
    * Really add seafdav
    * Fix some minor issues

--- docker/build.sh
+++ docker/build.sh
@@ -21,6 +21,8 @@ if [ "x$1" != "x" ]; then
     SEAFILE_VERSION=$1
 fi
 
+[ -z $LIBEVHTP_VERSION  ] && LIBEVHTP_VERSION="1.1.6"
+[ -z $LIBSEARPC_VERSION ] && LIBSEARPC_VERSION="3.1-latest"
 ##################################
 # Where we should install Seahub #
 ##################################
@@ -67,44 +69,37 @@ apk add bash openssl python py-setuptools py-imaging sqlite \
 
 #################################################
 # Add build-deps for Seafile-Server             #
-# I'm creatind variable with them to being able #
-#  to delete them back after building           #
 #################################################
-BUILD_DEP="curl-dev libevent-dev glib-dev util-linux-dev intltool \
+apk add --virtual .build_dep \
+    curl-dev libevent-dev glib-dev util-linux-dev intltool \
     sqlite-dev libarchive-dev libtool jansson-dev vala fuse-dev \
     cmake make musl-dev gcc g++ automake autoconf bsd-compat-headers \
-    python-dev file mariadb-dev mariadb-dev"
-apk add $BUILD_DEP
-#mariadb-dev - We'll make seafile without MySQL support
+    python-dev file mariadb-dev mariadb-dev py-pip
 
-###########################################################
-# Seahub dependencies -                                   #
-#  add py-pip and allow it to install all python packages #
-# To instal some of python modules we need gcc,           #
-#  so this stage should be AFTER adding build-deps        #
-###########################################################
-apk add py-pip 
 pip install django==1.8 pytz django-statici18n djangorestframework \
     django_compressor django-post_office gunicorn flup chardet \
     python-dateutil six openpyxl \
     django-picklefield
 pip install https://github.com/haiwen/django-constance/archive/bde7f7c.zip
 
+PYTHON_PACKAGES_DIR=`python -c "from distutils.sysconfig import get_python_lib; print(get_python_lib())"`
 
 ####################
 # Install libevhtp #
 ####################
-wget https://github.com/ellzey/libevhtp/archive/1.1.6.tar.gz -O- | tar xzf -
-cd libevhtp-1.1.6/ && cmake -DEVHTP_DISABLE_SSL=ON -DEVHTP_BUILD_SHARED=ON . && make && make install 
+wget https://github.com/ellzey/libevhtp/archive/${LIBEVHTP_VERSION}.tar.gz -O- | tar xzf -
+cd libevhtp-${LIBEVHTP_VERSION}/ && cmake -DEVHTP_DISABLE_SSL=ON -DEVHTP_BUILD_SHARED=ON . && make && make install 
 
 ###################################
 # Download all Seafile components #
 ###################################
 cd $WORK_DIR
-wget https://github.com/haiwen/libsearpc/archive/v3.1-latest.tar.gz -O- | tar xzf -
+wget https://github.com/haiwen/libsearpc/archive/v${LIBSEARPC_VERSION}.tar.gz -O- | tar xzf -
 wget https://github.com/haiwen/ccnet-server/archive/v${SEAFILE_VERSION}-server.tar.gz -O- | tar xzf -
 wget https://github.com/haiwen/seafile-server/archive/v${SEAFILE_VERSION}-server.tar.gz -O- | tar xzf -
 wget https://github.com/haiwen/seahub/archive/v${SEAFILE_VERSION}-server.tar.gz -O- | tar xzf -
+wget https://github.com/haiwen/seafobj/archive/v${SEAFILE_VERSION}-server.tar.gz -O- | tar xzf -
+wget https://github.com/haiwen/seafdav/archive/v${SEAFILE_VERSION}-server.tar.gz -O- | tar xzf -
 
 #####################################
 # Seahub is python application,     #
@@ -117,16 +112,18 @@ tar czf /usr/local/share/seafile/seahub.tgz -C $WORK_DIR/seahub-${SEAFILE_VERSIO
 ###############################
 # Build and install libSeaRPC #
 ###############################
-cd libsearpc-3.1-latest/ && ./autogen.sh && ./configure && make && make install
+cd libsearpc-${LIBSEARPC_VERSION}/ && ./autogen.sh && ./configure && make && make install
 
 export PKG_CONFIG_PATH=/usr/local/lib/pkgconfig
+
 ###########################
 # Build and install CCNET #
 ###########################
 cd $WORK_DIR/ccnet-server-${SEAFILE_VERSION}-server/ && \
 	./autogen.sh && \
-    ./configure --with-mysql --with-postgresql && \
+    ./configure --with-mysql --with-postgresql --enable-python && \
 		make && make install
+
 
 ####################################
 # Build and install Seafile-Server #
@@ -137,7 +134,7 @@ cd $WORK_DIR/ccnet-server-${SEAFILE_VERSION}-server/ && \
 cd $WORK_DIR/seafile-server-${SEAFILE_VERSION}-server/
 patch -p1 < /tmp/seafile-server.patch
 ./autogen.sh && \
-    ./configure --with-mysql --with-postgresql && \
+    ./configure --with-mysql --with-postgresql --enable-python && \
 		make && make install
 
 #Copy some useful scripts to /usr/local/bin
@@ -148,15 +145,24 @@ cp scripts/seaf-gc.sh /usr/local/bin/seafile-gc
 #mkdir -p /usr/local/share/seafile/
 mv scripts /usr/local/share/seafile/
 
+###########
+# SeafObj #
+###########
+cd $WORK_DIR/seafobj-${SEAFILE_VERSION}-server/
+mv seafobj ${PYTHON_PACKAGES_DIR}/
+
+###########
+# SeafDav #
+###########
+cd $WORK_DIR/seafdav-${SEAFILE_VERSION}-server/
+mv wsgidav ${PYTHON_PACKAGES_DIR}/
+
+echo "export PYTHONPATH=${PYTHON_PACKAGES_DIR}:/usr/local/lib/python2.7/site-packages/:${SEAFILE_SERVER_DIR}/seafile-server/seahub/thirdpart" >> /etc/profile.d/python-local.sh
+
 ldconfig || true
-
-echo "export PYTHONPATH=/usr/local/lib/python2.7/site-packages:${SEAFILE_SERVER_DIR}/seafile-server/seahub/thirdpart" >> /etc/profile.d/python-local.sh
-
 
 echo "Seafile-Server has been built successfully!"
 
-#WSGIdav is needed for seafdav
-pip install wsgidav
 ##############################
 # Do some preparations       #
 # Like add seafile user and  #
@@ -180,7 +186,7 @@ echo "seafile user has been created and configured successfully!"
 # Delete all unneded files and packages #
 #########################################
 cd /
-apk del --purge $BUILD_DEP
+apk del --purge .build_dep
 rm -rf $WORK_DIR
 rm /var/cache/apk/*
 rm -rf /root/.cache
@@ -189,3 +195,5 @@ rm -f /tmp/seafile-server.patch
 echo "unneded files were cleaned"
 
 echo "Done!"
+
+# vim: set ft=sh foldmethod=marker shiftwidth=4 tabstop=4 expandtab :
